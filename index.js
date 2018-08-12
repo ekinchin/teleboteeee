c53eac6ec625013dc5048999e0bd6e995bee1241
ekinchin <evgen4in@gmail.com>
2018-08-12 22:41:09 +0500

    ттмитмит

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*
 'use strict';
 
 const https = require('https'),
@@ -9,13 +8,8 @@ const https = require('https'),
   // hostname: '45.55.184.96',
   // port: 3128,
 const options = {
-  hostname: '104.139.71.127',
-  port: 58366,
-  path: host+path,
-  method: 'GET',
-  headers: {
-        Host: "api.telegram.org"
-    }
+  host: host,
+  path: path
 };
 
 
@@ -31,30 +25,4 @@ const req = https.request(options, (res) => {
 req.on('error', (e) => {
   console.error(e);
 });
-req.end();*/
-
-
-var Http = require('http');
-var Tls = require('tls');
-
-var req = Http.request({
-    host: '192.168.5.8',
-    port: 3128,
-    method: 'CONNECT',
-    path: 'twitter.com:443',
-});
-
-req.on('connect', function (res, socket, head) {
-    var cts = Tls.connect({
-    host: 'twitter.com',
-    socket: socket
-    }, function () {
-        cts.write('GET / HTTP/1.1rnHost: twitter.comrnrn');
-    });
-
-    cts.on('data', function (data) {
-        console.log(data.toString());
-    });
-});
-
 req.end();
