8851e04b2f234fec500330a8d835714684416cea
ekinchin <evgen4in@gmail.com>
2018-08-18 13:00:56 +0500

    123

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const https = require('https'),
-	token = "674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc",
-	host = "api.telegram.org",
-	path = "/bot" + token + "/getUpdates";
-
+token = "674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc",
+host = "api.telegram.org",
+path = "/bot" + token + "/getUpdates";
 
 const options = {
   hostname: host,
@@ -13,26 +12,27 @@ const options = {
   method: 'GET'
 };
 
-  var request = https.request(options, (res) => {
-    //console.log('statusCode:', res.statusCode);
-    //console.log('headers:', res.headers);
-    res.on('data', (data) => {
-      var answer=JSON.parse(data)
-      console.log(answer);
+var request = https.request(options, (res) => {
+  //console.log('statusCode:', res.statusCode);
+  //console.log('headers:', res.headers);
+  res.on('data', (data) => {
+    var answer=JSON.parse(data)
+    console.log(answer);
+    if(answer.result){      
       console.log('\n');
       console.log(answer.result[0].message.from);
       console.log('\n');
       console.log(answer.result[0].message.chat);
       console.log('\n');
       console.log(answer.result[0].message.entities);
-  
-    });
+    }
   });
+});
 
-  request.on('error', (e) => {
-    console.error(e);
-  });
-  request.end();
+request.on('error', (e) => {
+  console.error(e);
+});
+request.end();
 
 
 /*
