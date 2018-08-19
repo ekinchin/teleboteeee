5c3c6d106b23d9b2ffeb9e0901776a5701e7f75f
ekinchin <evgen4in@gmail.com>
2018-08-19 22:57:15 +0500

    123

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 'use strict';
 
-const https = require('https'),
-token = "674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc",
-host = "api.telegram.org",
-path = "/bot" + token + "/getUpdates";
+var fs = require('fs');
+var https = require('https'),
+  token = "674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc",
+  host = "api.telegram.org",
+  path = "/bot" + token + "/getUpdates";
+
+var webHookPath="https://salty-reaches-74004.herokuapp.com/674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc";
+var setwebhook="/bot"+token+"setwebhook?url="+webHookPath;
+const serverOptions = {
+  key: fs.readFileSync('public.pem'),
+  cert: fs.readFileSync('private.pem')
+}
 
 const options = {
   hostname: host,
   port: 443,
-  path: path,
+  path: setwebhook,
   method: 'GET'
 };
 
 var request = https.request(options, (res) => {
-  //console.log('statusCode:', res.statusCode);
-  //console.log('headers:', res.headers);
   res.on('data', (data) => {
     var answer=JSON.parse(data)
     console.log(answer);
-    if(answer.result.length!=0){      
-      console.log('\n');
-      console.log(answer.result[0].message.from);
-      console.log('\n');
-      console.log(answer.result[0].message.chat);
-      console.log('\n');
-      console.log(answer.result[0].message.entities);
-    }
   });
 });
 
@@ -33,18 +31,3 @@ request.on('error', (e) => {
   console.error(e);
 });
 request.end();
-
-
-/*
-ответ на /getUpdates - JSON
-{ok, message}
-message -> {
-  from, 'объект'
-  chat, 'объект'
-  date, 'дата'
-  text, (строка)
-  entities (array)
-}
-
-
-*/
