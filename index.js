fe74e5bf2333890d44d2fadbf886950bf7ea5da5
ekinchin <evgen4in@gmail.com>
2018-08-20 22:34:57 +0500

    123

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var port = process.env.PORT;
 var fs = require('fs');
-var https = require('https');
+var http = require('http');
 var token = "674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc";
 var telegram = "api.telegram.org";
 var path = "/bot" + token + "/getUpdates";
@@ -17,7 +17,7 @@ const options = {
   method: 'GET'
 };
 
-var request = https.request(options, (res) => {
+var request = http.request(options, (res) => {
   res.on('data', (data) => {
     var answer=JSON.parse(data)
     console.log(answer);
@@ -25,7 +25,7 @@ var request = https.request(options, (res) => {
 });
 request.end();
 
-var server = https.createServer();
+var server = http.createServer();
 server.listen(port);
 server.on('request',(req, res) => {
   console.log(req);
