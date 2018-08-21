6ee485d8a29e74d9b6b22431cddbccbbabee6ab6
ekinchin <evgen4in@gmail.com>
2018-08-21 21:53:21 +0500

    123

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
 'use strict';
 
-var port = process.env.PORT;
-var fs = require('fs');
 var http = require('http');
 var https = require('https');
+
 var token = "674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc";
 var telegram = "api.telegram.org";
-var path = "/bot" + token + "/getUpdates";
 
 var webHookPath="https://salty-reaches-74004.herokuapp.com/674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc";
-var setWebhookCMD="/bot"+token+"/setwebhook?url="+webHookPath;
 
-
-var webHooked=false;
-
-const options = {
-  hostname: telegram,
-  port: 443,
-  path: setWebhookCMD,
-  method: 'GET'
+var CMD = {
+  setWebHook:"setwebhook",
+  getUpdates:"getUpdates"
 };
 
-var request = https.request(options, (res) => {
-  res.on('data', (data) => {
-    (JSON.parse(data).result==true)?webHooked=true:webHooked=false;
+function setWebHook(telegram, token, webhookpath, command) {
+  return new Promise((resole,reject)=>{
+    var options = {
+      hostname: telegram,
+      port: 443,
+      path:"/bot"+token+"/"+command+"?url="+webhookpath,
+      method: 'GET'
+    };
+    console.log(options);
+    var request = https.request(options, (res) => {
+      res.on('data', (data) => {
+        result=JSON.parse(data).result
+        (result==true)?resolve(result):reject(result);
+      });
+    });
   });
-});
-request.end();
+}
 
-if(webHooked){
-  var server = http.createServer();
+function Server(){
+   var server = http.createServer();
   server.listen(port);
   server.on('request',(req, res) => {
     console.log(req);
     res.end('hello world\n');
-  });
-}else{
-  console.log('not set webhook');
 }
+
+var prmSetWebHook = setWebHook(telegram, token, webHookPath, CMD.setWebHook);
+prmSetWebHook.then(Server,
+  (error)=>{
+    console.log("ERROR",error);
+  }
+);
