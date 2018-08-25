32bd027bd99daa5da122802527aa6691004afa89
ekinchin <evgen4in@gmail.com>
2018-08-25 23:44:27 +0500

    проба с getMe JSON

--- index.js
+++ index.js
@@ -6,10 +6,38 @@ var https = require('https');
 var token = "674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc";
 var telegram = "api.telegram.org";
 var webHookPath="https://salty-reaches-74004.herokuapp.com/674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc";
+
 var CMD = {
   setWebHook:"setwebhook",
   getUpdates:"getUpdates"
 };
+
+function sendRequest(host, port, path,data) {
+  var options = {
+    hostname: host,
+    port: port,
+    path:path,
+    method:'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  return sendHttpRequest(options,data);
+}
+
+function sendHttpRequest(options,data) {
+
+  var request = https.request(options, (res) => {
+    res.on('data', (data) => {
+      return JSON.parse(data);
+    });
+  });
+  request.end(JSON.stringify(data));
+}
+
+
+
+console.log("TEST",sendRequest(telegram, 443, "/bot"+token,{"method": "getMe"}));
 
 function setWebHook(telegram, token, webhookpath, command) {
   return new Promise((resolve,reject)=>{
@@ -27,15 +55,17 @@ function setWebHook(telegram, token, webhookpath, command) {
         (result==true)?resolve(result):reject(result);
       });
     });
-    request.end();
+    request.end(JSON.stringify({"method": "sendMessage", "text": "hello", "chat_id":request.message.chat.id}));
   });
 }
 
-function reqParse(){
-
+function reqParse(request){
+  return new Promise((resolve,reject)=>{
+    console.log("request parsing");
+  });
 }
 
-function botResponse(){
+function responseGen(){
 
 }
 
@@ -49,14 +79,11 @@ function Server(){
     }
     req.on('data',(data)=>{
       var request=JSON.parse(data);
-      console.log(request.message.chat.id);
-      console.log(request.message.text);
-      console.log(request.message.entities);
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end(
-      JSON.stringify({"method": "sendMessage", "text": "hello", "chat_id":request.message.chat.id
-        }));
+      res.setHeader('Content-Type', 'application/json');
+      res.end(
+        JSON.stringify({"method": "sendMessage", "text": "hello", "chat_id":request.message.chat.id})
+        );
     });
   });
 }
