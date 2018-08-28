b55b6976c7dfec4c3a7f42f4f7ff04b05a0275a1
Evgenii Kinchin <kinchin@mail.ru>
2018-08-28 11:42:59 +0500

    123

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ var weatherPath = "/v1/informers?lat=57&lon=65&lang=ru_RU";
 var weatherHeader = {'X-Yandex-API-Key': "40f0e52b-168d-40a4-ba38-0c2bf4d98726"};
 
 var CMD = {
-  setWebHook:"setwebhook",
-  getUpdates:"getUpdates",
-  sendMessage:"sendMessage"
+	setWebHook:"setWebhook",
+	getUpdates:"getUpdates",
+	sendMessage:"sendMessage"
 };
 
 var bot_commands={
@@ -28,107 +28,104 @@ var bot_commands={
 	},
 	'/weather':{
 		descripion:'Погода',
-		handler:
-			function(host, path, header, chat_id){
-				sendHttpRequest(host, path, header)
-					.then((data)=>{
-						data=JSON.parse(data);
-						var answer="Текущая температура: " + data.fact.temp+'\n'
-							+"Ощущается как: " + data.fact.feels_like+'\n'
-							+"Ветер: " + data.fact.wind_speed;
-						sendJSONRequest(telegram,"/bot"+token+"/"+CMD.sendMessage, {"method": "sendMessage", "chat_id":chat_id, "text":answer});
-					},
-					(error)=>{
-						sendJSONRequest(telegram,"/bot"+token+"/"+CMD.sendMessage, {"method": "sendMessage", "chat_id":chat_id, "text":'Что-то не получилось :-('});
-					});
-			}
+		handler:weather
 	}
 };
 
+function weather(host, path, header, chat_id){
+	sendHttpRequest(host, path, header)
+	.then((data)=>{
+		data=JSON.parse(data);
+		var answer="Текущая температура: " + data.fact.temp+'\n'
+			+"Ощущается как: " + data.fact.feels_like+'\n'
+			+"Ветер: " + data.fact.wind_speed;
+		sendJSONRequest(telegram,"/bot"+token+"/"+CMD.sendMessage, {"method": "sendMessage", "chat_id":chat_id, "text":answer});
+	},
+	(error)=>{
+		sendJSONRequest(telegram,"/bot"+token+"/"+CMD.sendMessage, {"method": "sendMessage", "chat_id":chat_id, "text":'Что-то не получилось :-('});
+	});
+}
+
 function sendJSONRequest(host, path, data){
-  return new Promise((resolve,reject)=>{
-    var options = {
-      hostname: host,
-      port: 443,
-      path:path,
-      method:'POST',
-      headers:{
-      	'Content-Type':'application/json'
-      }
-    };
-    const req = https.request(options, (res) => {
-      var answer='';
-      res.on('data', (data) => {
-        answer+=data;
-      });
-      res.on('end',()=>{
-      	console.log(answer);
+	return new Promise((resolve,reject)=>{
+		var options = {
+			hostname: host,
+			port: 443,
+			path:path,
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json'
+			}
+		};
+		const req = https.request(options, (res) => {
+			var answer='';
+			res.on('data', (data) => {
+				answer+=data;
+			});
+			res.on('end',()=>{
         //resolve(answer);
-      });
-      res.on('error',()=>{
-      	console.log(answer);
-        //reject(answer);
-      })
     });
-    req.write(JSON.stringify(data));
-    req.end();
-  });
+			res.on('error',()=>{
+        //reject(answer);
+    })
+		});
+		req.write(JSON.stringify(data));
+		req.end();
+	});
 }
 
 function sendHttpRequest(host, path,header){
-  return new Promise((resolve,reject)=>{
-    var options = {
-      hostname: host,
-      port: 443,
-      path:path,
-      method:'GET'
-    };
-    if(header!=undefined){
-    	options.headers=header;
-    }
-    https.request(options, (res) => {
-      var answer='';
-      res.on('data', (data) => {
-        answer+=data;
-      });
-      res.on('end',()=>{
-        resolve(answer);
-      });
-      res.on('error',()=>{
-        reject(answer);
-      })
-    }).end();
-  });
+	return new Promise((resolve,reject)=>{
+		var options = {
+			hostname: host,
+			port: 443,
+			path:path,
+			method:'GET'
+		};
+		if(header!=undefined){
+			options.headers=header;
+		}
+		https.request(options, (res) => {
+			var answer='';
+			res.on('data', (data) => {
+				answer+=data;
+			});
+			res.on('end',()=>{
+				resolve(answer);
+			});
+			res.on('error',()=>{
+				reject(answer);
+			})
+		}).end();
+	});
 }
 
 function reqParse(data){
-  data=JSON.parse(data);
-  var text = data.message.text;
-  var entities={};
-  (data.message.entities==undefined)?null:entities = data.message.entities[0];
-  var answer = {"method": "sendMessage", "chat_id":data.message.chat.id, "text":''};
+	data=JSON.parse(data);
+	var text = data.message.text;
+	var entities={};
+	(data.message.entities==undefined)?null:entities = data.message.entities[0];
+	var answer = {"method": "sendMessage", "chat_id":data.message.chat.id, "text":''};
 
-  if(entities.type=='bot_command'){
-    switch(text.split(' ')[0].toLowerCase()) {
-      case '/start':
-        answer["text"]="Hello, "+ data.message.from.first_name;
-        break;
-      case '/help':
-        answer["text"]="/start - поздороваться\n/weather - текущая погода\n/help - эта справка";
-        break;
-      case '/weather':
-      	bot_commands['/weather'].handler(weatherHost, weatherPath, weatherHeader, data.message.chat.id);
-      	break;
-      default:
-        answer["text"]="Я не знаю такой команды, "+data.message.from.first_name;
-        break;
-    };
-  }else{
-    return 0;
-  }
-  console.log(answer);
-  return JSON.stringify(answer);
-//JSON.stringify({"method": "sendMessage", "text": request, "chat_id":request.message.chat.id})
+	if(entities.type=='bot_command'){
+		switch(text.split(' ')[0].toLowerCase()) {
+			case '/start':
+			answer["text"]="Hello, "+ data.message.from.first_name;
+			break;
+			case '/help':
+			answer["text"]="/start - поздороваться\n/weather - текущая погода\n/help - эта справка";
+			break;
+			case '/weather':
+			bot_commands['/weather'].handler(weatherHost, weatherPath, weatherHeader, data.message.chat.id);
+			break;
+			default:
+			answer["text"]="Я не знаю такой команды, "+data.message.from.first_name;
+			break;
+		};
+	}else{
+		return 0;
+	}
+	return JSON.stringify(answer);
 }
 
 function botResponse(){
@@ -136,25 +133,25 @@ function botResponse(){
 }
 
 function Server(){
-  console.log("OK, starting server....")
-  var server = http.createServer();
-  server.listen(process.env.PORT);
-  server.on('request',(request, response) => {
-    if(request.url==('/'+token)){
-      console.log("Telegram request");
-    }
-    request.on('data',(data)=>{
-      var result=reqParse(data);
-      if(result!=0){
-        response.setHeader('Content-Type', 'application/json');
-        response.write(result);
-      }
-      response.end();
-    });
-  });
+	console.log("OK, starting server....")
+	var server = http.createServer();
+	server.listen(process.env.PORT);
+	server.on('request',(request, response) => {
+		if(request.url==('/'+token)){
+			console.log("Telegram request");
+		}
+		request.on('data',(data)=>{
+			var result=reqParse(data);
+			if(result!=0){
+				response.setHeader('Content-Type', 'application/json');
+				response.write(result);
+			}
+			response.end();
+		});
+	});
 }
 
 sendHttpRequest(telegram, "/bot"+token+"/"+CMD.setWebHook+"?url="+webHookPath)
 .then(Server,(error)=>{
-  console.log("ERROR",error);
+	console.log("ERROR",error);
 });
