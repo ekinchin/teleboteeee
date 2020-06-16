1196cf158ff65273cab4e4c2532add98e613eab5
ekinchin <evgen4in@gmail.com>
2020-06-16 23:37:31 +0500

    Revert "удалены токены. В любом случае они давно заблокированы."
    This reverts commit 0f78095afd63906ed47b51d7952bdf7ed6224cf2.

--- src/keys.js
+++ src/keys.js
@@ -1,2 +1,2 @@
-export const yandexWeatherToken = '****';
-export const telegramToken = '****';
+export const yandexWeatherToken = '40f0e52b-168d-40a4-ba38-0c2bf4d98726';
+export const telegramToken = '674082318:AAG4e5AXQu_SbJkYSVji4chwaiggtGrMLBc';
