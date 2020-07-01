a6c4a934206be01c01f6fb74c443c70ec33d6860
ekinchin <evgen4in@gmail.com>
2020-07-01 22:02:10 +0500

    прототип разбора команд

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,3 +4,11 @@
 // eventer.on('/start', botCommands['/start'].handler);
 // eventer.on('/help', botCommands['/help'].handler);
 // eventer.on('undefined', botCommands.undefined.handler);
+
+const commands = {
+  '/start': () => 'start command',
+  '/help': () => 'help command',
+  '/undefined': () => 'undefined command',
+};
+
+export default (command, payload) => (command in commands ? commands[command](payload) : commands['/undefined']());
