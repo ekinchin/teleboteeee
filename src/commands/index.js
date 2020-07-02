457cf4dbb5f7e5fadb28e5d3987bdf2b865b3d4c
ekinchin <evgen4in@gmail.com>
2020-07-02 22:53:54 +0500

    загрузка команд

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,14 +1,19 @@
-// import { EventEmitter } from 'events';
-// const eventer = new EventEmitter();
-// eventer.on('/weather', botCommands['/weather'].handler);
-// eventer.on('/start', botCommands['/start'].handler);
-// eventer.on('/help', botCommands['/help'].handler);
-// eventer.on('undefined', botCommands.undefined.handler);
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+import fs from 'fs';
 
-const commands = {
-  '/start': () => 'start command',
-  '/help': () => 'help command',
-  '/undefined': () => 'undefined command',
+const commandsLoad = () => {
+  const files = fs.readdirSync('./dist/commands');
+  const modules = files.filter(filename => filename !== 'index.js');
+  return modules.reduce((commands, file) => {
+    const commandname = `/${file.slice(0, -3)}`;
+    const modulepath = `./${file}`;
+    const module = require(modulepath).default;
+    // eslint-disable-next-line no-param-reassign
+    if (module) commands[commandname] = module;
+    return commands;
+  }, {});
 };
 
+const commands = commandsLoad();
 export default (command, payload) => (command in commands ? commands[command](payload) : commands['/undefined']());
