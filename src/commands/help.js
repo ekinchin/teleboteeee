afce6622f9b504248594e96d7faa70ba28bf493d
ekinchin <evgen4in@gmail.com>
2020-07-03 22:43:02 +0500

    частично реализована команда /help
    TODO: необходимо динамически определять список команд и их описание

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,15 +1,10 @@
-import { URL } from 'url';
-import request from '../httpRequest';
-
-const { TELEGRAM_TOKEN } = process.env;
-
-const telegramUrl = new URL('https://api.telegram.org');
-telegramUrl.pathname = `bot${TELEGRAM_TOKEN}/sendMessage`;
+import { sendMessage } from '../telegram';
 
 export default {
   descripion: 'Помощь',
-  handler: async (chatId) => {
+  run: async (payload) => {
     const answer = '/start - поздороваться\n/weather - текущая погода\n/help - эта справка';
-    await request(telegramUrl, { 'Content-Type': 'application/json' }, { method: 'sendMessage', chat_id: chatId, text: answer }, 'POST');
+    const result = await sendMessage(payload.chat.id, answer);
+    return result;
   },
 };
